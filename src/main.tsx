b094e0e8fe6f02c3e6a989006f7bdd4f077f268b
wenke.lai <wenke.studio@gmail.com>
2024-06-16 00:56:47 +0800

    build: import react-icons

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { IconContext } from "react-icons";
 import App from "./App.tsx";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <IconContext.Provider value={{ className: "text-3xl" }}>
+      <App />
+    </IconContext.Provider>
   </React.StrictMode>
 );
