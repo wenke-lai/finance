1e5a82fad2519ec35b92069a6ff6110fa881d7bb
wenke.lai <wenke.studio@gmail.com>
2024-06-16 22:42:24 +0800

    fix(pages): enhance the home layout with new layout components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Auth } from "src/pages/auth";
-import Home from "src/pages/home";
+import Home from "src/pages/dashboard";
+import PageLayout from "./pages/page-layout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home.Layout />,
+    element: <PageLayout />,
     children: [
       { index: true, element: <Home.Overview /> },
       { path: "expense", element: <div>Expense</div> },
