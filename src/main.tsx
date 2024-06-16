476e6c81295861b3e3c5e817c895c82156778b37
wenke.lai <wenke.studio@gmail.com>
2024-06-17 00:02:02 +0800

    fix: compatible between Tailwind CSS and AntD

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { px2remTransformer, StyleProvider } from "@ant-design/cssinjs";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { IconContext } from "react-icons";
@@ -5,10 +6,16 @@ import App from "./App.tsx";
 
 import "./index.css";
 
+const px2rem = px2remTransformer({
+  rootValue: 16, // 16px = 1rem; @default 16
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <IconContext.Provider value={{ className: "text-3xl" }}>
-      <App />
-    </IconContext.Provider>
+    <StyleProvider transformers={[px2rem]} layer>
+      <IconContext.Provider value={{ className: "text-3xl" }}>
+        <App />
+      </IconContext.Provider>
+    </StyleProvider>
   </React.StrictMode>
 );
